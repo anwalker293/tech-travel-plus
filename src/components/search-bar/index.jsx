5cded05f27c345b3ba4aea4d2b3938a3492415ca
Alex Walker <alex.walker@indicio.tech>
2023-01-25 17:53:16 +0100

    feat: search bar
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

---
+++ src/components/search-bar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SearchBar = ({ value, handleSearchKey, clearSearch, formSubmit }) => {
+  return (
+    <div className="searchBar-wrap">
+      <form onSubmit={formSubmit}>
+        <input
+          type="text"
+          onChange={handleSearchKey}
+          placeholder="Search by Category"
+          value={value}
+        />
+        {value && <span onClick={clearSearch}>X</span>}
+        <button>Go</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header/index.jsx";
+import SearchBar from "../../components/search-bar/index.jsx";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   );
 };
