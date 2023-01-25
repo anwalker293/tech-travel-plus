ca141d8556deaef8aa1fd5a7362e898c2a39a1b8
Alex Walker <alex.walker@indicio.tech>
2023-01-25 17:35:59 +0100

    feat: begin routes
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import React from "react";
 import "./App.css";
+import Blog from "./pages/blog/index";
+import Home from "./pages/home/index.jsx";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>HI hi</h1>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
     </div>
   );
 };
