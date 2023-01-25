ceadcbfe3a2d83407a1003537df0297d235c2499
Alex Walker <alex.walker@indicio.tech>
2023-01-25 18:41:20 +0100

    feat: util scratch
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,10 +4,39 @@ import BlogList from "../../components/blogList/index.jsx";
 import { blogList } from "../../config/data.js";
 
 const Home = () => {
+  const [blog, setBlogs] = useState(initialState);
+  const [searchKey, setSearchKey] = useState("");
+
+  // Search submit
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    handleSearchResults();
+  };
+
+  // Search for blogs by category
+  const handleSearchSubmit = () => {
+    const allBlogs = blogList;
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
+    );
+
+    setBlogs(filteredBlogs);
+  };
+
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
+
   return (
     <div>
       <Header />
-      <SearchBar />
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handleSearchSubmit}
+        handleSearchKey={(e) => setSearchKey(e.target.value)}
+      />
       <BlogList blogs={blogList} />
     </div>
   );
