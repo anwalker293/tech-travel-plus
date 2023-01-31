2072058b2e1662b82149850e342f645b1585bd2a
Alex Walker <alex.walker@indicio.tech>
2023-01-31 08:40:29 +0100

    fix: simple media query for mobile
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,9 @@ import "./styles.css";
 const Header = () => (
   <header className="home-header">
     <h1>
-      <span>“</span> Tech & Travel <span>”</span>
+      <span>“</span>
+      <div className="header-text">Tech & Travel</div>
+      <span>”</span>
     </h1>
     <p>
       blog dedicated to those taking an adventure, at home or abroad <br />❤
