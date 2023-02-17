72fe02369ac9797beb956312ebbc6ab53f66b1f7
Alex Walker <alex.walker@indicio.tech>
2023-02-17 20:06:44 +0100

    feat: optimize images, add Indicio blog
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/components/imageSlider/index.jsx
+++ src/components/imageSlider/index.jsx
@@ -1,11 +1,9 @@
 import "./styles.css";
 import img1 from "../../images/tech-travel/train1.png";
-import img4 from "../../images/albania/albania2.png";
 
 import img2 from "../../images/albania/albania4.png";
-import img7 from "../../images/tech-travel/tech.png";
-import img5 from "../../images/tirana/tirana3.png";
-import img6 from "../../images/albania/albania6.png";
+import img5 from "../../images/tech-travel/tech.png";
+import img4 from "../../images/albania/albania6.png";
 import img3 from "../../images/albania/albania8.png";
 
 const urlBois = [
@@ -23,12 +21,6 @@ const urlBois = [
   },
   {
     url: img5,
-  },
-  {
-    url: img6,
-  },
-  {
-    url: img7,
   },
 ];
 
--- src/config/data.js
+++ src/config/data.js
@@ -1,5 +1,131 @@
 export const blogList = [
   {
+    id: 2,
+    title:
+      "Quickly Test and Develop your ACA-Py Code with Indicio’s New Minimal Example Repo",
+    category: "tech",
+    subCategory: ["aca-py"],
+    description: (
+      <div>
+        <h3>
+          Coding in ACA-Py just got a whole lot easier with Indicio’s a simple,
+          hackable repo
+        </h3>
+        <br />
+        Indicio recently unveiled its{" "}
+        <a
+          href="https://github.com/Indicio-tech/acapy-minimal-example"
+          target="_blank"
+        >
+          Minimal Example Repo
+        </a>
+        , built with both experienced developers and those just starting out and
+        in mind.
+        <br />
+        <br />
+        <h4>What is it?</h4> Indicio’s{" "}
+        <a
+          href="https://github.com/Indicio-tech/acapy-minimal-example"
+          target="_blank"
+        >
+          Minimal Example repo (AME)
+        </a>{" "}
+        is a simple, hackable repo containing the building blocks to quickly
+        reproduce an issue or demonstrate a feature within ACA-Py.
+        <br />
+        <br />
+        <h4>Who is it for?</h4> Developers and testers (especially those new to
+        ACA-Py) who are looking to build scenarios with ACA-Py will find the
+        repo very useful, whether they are seeking to quickly reproduce an issue
+        (such as revocation, trust pings, issuing and verifying a simple
+        credential, or sending messages between agents) or to demonstrate a
+        feature by writing one simple script or Pytest test. The repo cuts down
+        on the amount of time searching through documentation, while also being
+        approachable to developers new to ACA-Py. Instead of looking through all
+        the Aries RFCs, Indicio’s Aca-Py Minimal Example will help autofill
+        certain scenarios for you.
+        <br />
+        <br />
+        <h4>How does it make using ACA-Py faster / convenient?</h4> It gives you
+        a significant head start in replicating scenarios in ACA-Py. For
+        example, a traditional exchange between an Alice and Bob¹ agent can be
+        found in `minimal_example.py.` It allows you to quickly copy, paste, and
+        modify the components you need to get your scenario to work, without all
+        of the hassle of sorting through the Aries RFCs documentation.
+        <br />
+        <br />
+        <h4>What is available now?</h4> Presently, we have examples constructed,
+        with revocation support, for a traditional Alice and Bob agent scenario,
+        supporting protocol versions 1 and 2, and are working to add more
+        functionality. Further work will include generators for other common
+        agent setups (`Dockerfile`s + `docker-compose.yml`) such as:
+        <ul>
+          <li>Alice, Bob, Mediator</li>
+          <li>Issuer, Holder, Verifier</li>
+          <li>Endorser, Issuer, Holder, Verifier</li>
+          <li>
+            ACA-Py, Echo (Remote controlled static agent; for sending raw
+            messages)
+          </li>
+          <li>
+            Any combination of the above with a specified set of plugins
+            installed
+          </li>
+          <li>Any combination of the above with tails server and/or tunnel</li>
+        </ul>
+        <br />
+        <br />
+        <h4>
+          <a
+            href="https://github.com/Indicio-tech/acapy-minimal-example"
+            target="_blank"
+          >
+            Get Started Today
+          </a>
+        </h4>
+        In just three simple steps, the{" "}
+        <a
+          href="https://github.com/Indicio-tech/acapy-minimal-example"
+          target="_blank"
+        >
+          AME repo
+        </a>{" "}
+        can reduce the amount of code you need to write and save you time:
+        <br />
+        <br />
+        <i>A.) Determine the scenario you are writing. </i>
+        <br />
+        What does this flow look like? What all needs to happen in this flow?
+        <br />
+        <br />
+        <i>
+          B.) Determine if the AME repo fully or partly supports that scenario.
+        </i>
+        <br />
+        Is the scenario you are looking for already written?
+        <br />
+        <br />
+        <i>
+          C.) Copy and paste the portions that are relevant to the scenario you
+          are working on and modify it accordingly.
+        </i>
+        <br />
+        <br />
+        If the steps you are looking for aren’t already in `minimal_example.py`,
+        try searching in `protocols.py`
+        <br />
+        <br />¹ Aries agents are software components that act on behalf of
+        entities—people, organizations and things. These agents enable
+        decentralized, self-sovereign identity based on a secure, peer-to-peer
+        communications channel.
+      </div>
+    ),
+    authorName: "Alexandra N. Walker",
+    authorAvatar: "/assets/images/alex-optimize.jpeg",
+    createdAt: "February 7th, 2023",
+    cover: "/assets/images/blog2-photo1-optimize.jpeg",
+  },
+  {
     id: 1,
     title: "Complete 7-Day Itinerary for Exploring South Albania",
     category: "travel",
@@ -192,8 +318,8 @@ export const blogList = [
       </div>
     ),
     authorName: "Alexandra N. Walker",
-    authorAvatar: "/assets/images/alex-optimize.png",
+    authorAvatar: "/assets/images/alex-optimize.jpeg",
     createdAt: "January 30th, 2023",
-    cover: "/assets/images/albania-7day-itinerary.png",
+    cover: "/assets/images/albania-7day-itinerary-optimize.png",
   },
 ];
