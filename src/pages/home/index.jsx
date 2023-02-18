679ebb0893a130a4859a211ca7375d18a14d11a8
Alex Walker <alex.walker@indicio.tech>
2023-02-18 18:58:31 +0100

    fix: optimize images, remove metadata, decrease quality
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,8 +5,8 @@ import Header from "../../components/header/index.jsx";
 import SearchBar from "../../components/search-bar/index.jsx";
 import { blogList } from "../../config/data";
 import Slider from "../../components/Slider/index.jsx";
-import logo from "../../images/tech-travel/logo-optimize.png";
-import logoRight from "../../images/tech-travel/logo-full-wo-background-optimize.png";
+import logo from "../../images/tech-travel/logo-optimize copy.png";
+import logoRight from "../../images/tech-travel/logo-full-wo-background-optimize copy.png";
 import "./styles.css";
 
 const Home = () => {
